306a8c09e16c4c4d01caa6ee282480d2e13438ee
Elvis Chagas <elvischagas123@gmail.com>
2019-09-23 22:17:44 -0300

    commit trazendo os pordutos com estilo css

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,7 +2,13 @@ import React, {Component} from 'react'
 
 import api from '../../services/api'
 
+import './styles.css'
+
 class Main extends Component{
+    state = {
+        products: []
+    }
+    
     componentDidMount(){
         this.loadProducts()
     }
@@ -10,10 +16,23 @@ class Main extends Component{
     loadProducts = async () => {
         const response = await api.get('/products')
 
-        console.log(response.data.docs)
+        this.setState({ products: response.data.docs})
     }
+
+    
     render(){
-        return <h1>Hello Rocketseat</h1>
+        const { products } = this.state
+        return (
+            <div className="product-list">
+               { products.map(product =>(
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+                        <a href="#">Acessar</a>
+                    </article>
+                    ))}
+            </div>
+        )
     }
 }
 
